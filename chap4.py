d961300f5490c317524bef467befde349948e68b
Raj Singh <rajsing@bluepi.in>
2026-01-23 12:42:51 +0530

    s

--- chap4.py
+++ chap4.py
@@ -1,6 +1,6 @@
 # Type casting means converting a value from one data type to another.
 
-# .in simple definition 
+# .in simple definition
 # When you change the type of data (like number → string, string → number), that is called type casting.
 
 # Why do we need type casting?
@@ -15,30 +15,33 @@ x = "10"
 y = int(x)
 
 print(y)
-print(type(y))   # <class 'int'>
+print(type(y))  # <class 'int'>
 
 
+a = "raj singh"
+print(a)
+print(type(a))
 
 # Integer → String
 x = 100
 y = str(x)
 
 print(y)
-print(type(y))   # <class 'str'>
+print(type(y))  # <class 'str'>
 
 
 # Integer → Float
 x = 5
 y = float(x)
 
-print(y)         # 5.0
+print(y)  # 5.0
 
 
 # Float → Integer
 x = 9.8
 y = int(x)
 
-print(y)         # 9 (decimal part removed)
+print(y)  # 9 (decimal part removed)
 
 
 # String → Float
@@ -48,7 +51,7 @@ y = float(x)
 print(y)
 
 # Boolean Casting
-print(bool(1))       # True
-print(bool(0))       # False
-print(bool(""))      # False
-print(bool("Hello")) # True
+print(bool(1))  # True
+print(bool(0))  # False
+print(bool(""))  # False
+print(bool("Hello"))  # True
---
+++ chapt5.py
@@ -0,0 +1,14 @@
+ """Input Function
+
+ In Python, the input() function is used to take input from the user through the keyboard.
+
+ Basic definition
+
+ input() reads what the user types, converts it into a string, and returns it."""
+
+a =int(input ("enter number a:"))
+b = int(input("enter number b: "))
+print  ("Number a is :",a)
+print  ("Number b is :",b)
+
+print ("sum is ", a+b)
